6f213dfbd1d6f1e0fdf6d779fbe50b71555178d0
DIP THAKOR <dipthakor25@outlook.com>
2024-08-21 15:38:04 +0530

    End of Section 5 - Sorting,Filtering,Searching, Pagination

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -18,11 +18,12 @@ namespace Core.Interfaces
         Task<TResult?> GetEntityWithSpec<TResult>(ISpecification<T,TResult> spec);
         Task<IReadOnlyList<TResult>> ListAsync<TResult>(ISpecification<T,TResult> spec);
 
-
         void Add(T entity);
         void Update(T entity); 
         void Remove(T entity);  
         Task<bool> SaveAllAsync();
         bool Exists(int id);
+
+        Task<int> CountAsync(ISpecification<T> spec);
     }
 }
